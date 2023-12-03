0cf94df6b827303e67ebe4c28add3acfa276e6e5
Gary <chihsien05@gmail.com>
2023-12-03 15:38:22 +0800

    this version is work well

--- python/test.py
+++ python/test.py
@@ -1,30 +1,43 @@
 
-from selenium.webdriver.common.by import By
 
 # 1.匯入selenium
 from selenium import webdriver
+
+import time
+from selenium.webdriver.common.by import By
+#from pynput.keyboard import Key, Controller
 
 
 # 2.開啟瀏覽器
 driver = webdriver.Chrome()
 
+#driver.implicitly_wait(5)
+
 # 3.開啟註冊A頁面(頁面地址根據自己的需要修改)
-url = "file:///D:/SourceControl/Github/mysite/fileupload.html"
+url = "http://biz.coolsax.work/python/fileupload.html"
 driver.get(url)
 #driver.maximize_window()
 
-# 4.定位上傳檔案按鈕
-#upfile = driver.find_element_by_name("upfile")
-#upfile = driver.find_element("name","upfile")
-upfile = driver.find_elements(By.NAME,"upfile")
+time.sleep(2)
 
-#elem = driver.find_element_by_xpath('//*[@id="upfile"]')
-#id = (elem.text)
-#print(id)
+# 4.定位上傳檔案按鈕
+driver.find_element(By.ID,"file-upload").send_keys("D:\\測試上傳檔案.txt");
+time.sleep(2)
+
+driver.find_element(By.NAME, "btnUpload").click()
+
+time.sleep(2)
+
+if driver.page_source.find("Not Allowed"):
+    print ("OK")
+else:
+    print ("Upload Failed")    
+
+time.sleep(2)
 
 
 # 5.使用send_keys方法上傳檔案
 #upfile.send_keys(r"D:\測試上傳檔案.txt")
 
 # 6.關閉瀏覽器
-# driver.quit()
+driver.quit()

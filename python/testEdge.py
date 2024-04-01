50f199539d7eda528d8859935a97977acba7bf09
Gary Chen <chihsien05@gmail.com>
2024-04-01 10:54:45 +0800

    python file run at background silient mode test ok

--- python/testEdge.py
+++ python/testEdge.py
@@ -4,12 +4,20 @@
 from selenium import webdriver
 
 import time
+from datetime import datetime
 from selenium.webdriver.common.by import By
 #from pynput.keyboard import Key, Controller
 
+# write log
+path = 'output.txt'
+f = open(path, 'a')
 
 # 2.開啟瀏覽器
 driver = webdriver.Edge()
+
+step1 = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+f.write(step1 + ' 開啟瀏覽器' )
+f.write("\n" )
 
 #driver.implicitly_wait(5)
 
@@ -18,15 +26,29 @@ url = "http://biz.coolsax.work/python/fileupload.html"
 driver.get(url)
 #driver.maximize_window()
 
-time.sleep(5)
+step2 = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+f.write(step2 + ' 開啟註冊A頁面' )
+f.write("\n" )
+
+time.sleep(2)
 
 # 4.定位上傳檔案按鈕
 driver.find_element(By.ID,"file-upload").send_keys("D:\\測試上傳檔案.txt");
-time.sleep(5)
+time.sleep(2)
 
+step3 = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+f.write(step3 + ' 定位上傳檔案按鈕' )
+f.write("\n" )
+
+# 按下上傳Button
 driver.find_element(By.NAME, "btnUpload").click()
+time.sleep(2)
 
-time.sleep(5)
+step4 = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+f.write(step4 + ' 按下上傳Button' )
+f.write("\n" )
+
+f.close()
 
 if driver.page_source.find("Not Allowed"):
     print ("OK")
--- python/testEdgeBackground.py
+++ python/testEdgeBackground.py
@@ -8,9 +8,18 @@ from datetime import datetime
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options 
 
+# write log
+path = 'output.txt'
+f = open(path, 'a')
+
 options = Options()
 options.add_argument("headless")
 driver = webdriver.Edge(options = options)
+
+step1 = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+f.write(step1 + ' Silent Mode 開啟瀏覽器' )
+f.write("\n" )
+
 
 #driver.implicitly_wait(5)
 
@@ -19,28 +28,32 @@ url = "http://biz.coolsax.work/python/fileupload.html"
 driver.get(url)
 #driver.maximize_window()
 
+step2 = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+f.write(step2 + ' 開啟註冊A頁面' )
+f.write("\n" )
 
 # 4.定位上傳檔案按鈕
 driver.find_element(By.ID,"file-upload").send_keys("D:\\測試上傳檔案.txt");
+step3 = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+f.write(step3 + ' 定位上傳檔案按鈕' )
+f.write("\n" )
+
 time.sleep(2)
 
+# 按下上傳Button
 driver.find_element(By.NAME, "btnUpload").click()
 
-time.sleep(2)
+step4 = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+f.write(step4 + ' 按下上傳Button' )
+f.write("\n" )
+
+f.close()
+
 
 if driver.page_source.find("Not Allowed"):
     print ("OK")
 else:
     print ("Upload Failed")    
-
-
-# write log
-date_time = datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
-path = 'output.txt'
-f = open(path, 'a')
-f.write(date_time )
-f.write("\n" )
-f.close()
 
 
 # 5.使用send_keys方法上傳檔案
